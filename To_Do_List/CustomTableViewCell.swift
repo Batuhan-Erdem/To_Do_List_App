4e6256da3fa4c0858952eb6b1021570130dc6f9c
Batuhan-Erdem <erdembatuhan00069@gmail.com>
2025-01-16 13:34:03 +0300

    First page design finished

---
+++ To_Do_List/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  To_Do_List
+//
+//  Created by Batuhan Erdem on 14.01.2025.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var taskLabel: UILabel!
+    
+}
--- To_Do_List/ViewController.swift
+++ To_Do_List/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+   
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
+        cell.taskLabel.text = "test"
+        cell.iconImageView.image = UIImage(named: "circle.png")
+        
+        return cell
+    }
 }
 
